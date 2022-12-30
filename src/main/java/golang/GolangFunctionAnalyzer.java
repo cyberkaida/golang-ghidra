b64f992e50620572c83958208bccb88cf3d23faf
かいだ <kaida@cyberkaida.gg>
2022-12-30 17:40:55 +0900

    Make module reentrant, add comments through code

--- src/main/java/golang/GolangFunctionAnalyzer.java
+++ src/main/java/golang/GolangFunctionAnalyzer.java
@@ -10,6 +10,7 @@ import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.options.OptionType;
 import ghidra.framework.options.Options;
+import ghidra.program.flatapi.FlatProgramAPI;
 import ghidra.program.model.address.Address;
 import ghidra.program.model.address.AddressIterator;
 import ghidra.program.model.address.AddressSetView;
@@ -88,7 +89,7 @@ public class GolangFunctionAnalyzer implements Analyzer {
 		ArrayList<DataType> type_list = new ArrayList<>();
 		program.getDataTypeManager().findDataTypes(name, type_list);
 		if (type_list.size() == 0) {
-			throw new Exception("DataType could not be found. Is the data type defined in your program's DataTypes?");
+			throw new Exception("DataType " + name + " could not be found. Is the data type defined in your program's DataTypes?");
 		}
 		
 		return type_list.get(0);
@@ -109,20 +110,23 @@ public class GolangFunctionAnalyzer implements Analyzer {
 		return golang_pclntab_magic;
 	}
 
+	private DataType getGolangModuleStructureDataType(Program program) throws Exception {
+		// https://github.com/golang/go/blob/5639fcae7fee2cf04c1b87e9a81155ee3bb6ed71/src/runtime/symtab.go#L415
+		throw new NotYetImplementedException();
+	}
+
 	private DataType getGolangPcheaderStructureDataType(Program program) throws Exception {
 		CategoryPath golang_category_path = new CategoryPath(CategoryPath.ROOT, "Golang");
 		DataType golang_pcheader = program.getDataTypeManager().getDataType("/Golang/GolangPcheaderV0");
 		if (golang_pcheader == null ) {
 			Category golang_category = program.getDataTypeManager().createCategory(golang_category_path);
 
-			DataType uint8_t = this.findFirstDataType(program, "char"); // For the moment this is a char, but it should really be a uint8_t
 			ByteDataType byte_datatype = new ByteDataType();
 			IntegerDataType int_datatype = new IntegerDataType();
 			UnsignedIntegerDataType uint_datatype = new UnsignedIntegerDataType();
 
-
 			// TODO: Use the right pointer data type
-			DataType generic_pointer = this.findFirstDataType(program, "void *");
+			DataType generic_pointer = new PointerDataType();
 			//DataType uint32_t = this.findFirstDataType(program, "uint32_t");
 			//DataType uintptr_t = this.findFirstDataType(program, "uintptr_t");
 
@@ -131,10 +135,15 @@ public class GolangFunctionAnalyzer implements Analyzer {
 					"GolangPcheaderV0",
 					0
 			);
+			// Now let us fill in the structure
+			// This structure is part of the go compiler output. The magic changes with Golang compiler versions.
+			// This method implements the following:
+			// https://github.com/golang/go/blob/5639fcae7fee2cf04c1b87e9a81155ee3bb6ed71/src/runtime/symtab.go#L395
+			// https://github.com/golang/go/blob/f2656f20ea420ada5f15ef06ddf18d2797e18841/src/runtime/symtab.go#L407
 			DataType magic_data_type = this.getGolangPclntabMagicEnumDataType(program);
 			golang_pcheader_struct.add(magic_data_type, magic_data_type.getLength(), "magic", "The pclntab magic, as defined in src/runtime/symtab.go");
-			golang_pcheader_struct.add(uint8_t, uint8_t.getLength(), "pad0", "First padding byte");
-			golang_pcheader_struct.add(uint8_t, uint8_t.getLength(), "pad1", "Second padding byte");
+			golang_pcheader_struct.add(byte_datatype, byte_datatype.getLength(), "pad0", "First padding byte");
+			golang_pcheader_struct.add(byte_datatype, byte_datatype.getLength(), "pad1", "Second padding byte");
 			golang_pcheader_struct.add(byte_datatype, byte_datatype.getLength(), "minLC", "min instruction size");
 			golang_pcheader_struct.add(byte_datatype, byte_datatype.getLength(), "ptrSize", "size of a ptr in bytes");
 			// TODO: Validate if our analysis is correct, compare the ptrSize to what Ghidra thinks the pointer size is
@@ -156,11 +165,24 @@ public class GolangFunctionAnalyzer implements Analyzer {
 	public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log)
 			throws CancelledException {
 			try {
+				FlatProgramAPI api = new FlatProgramAPI(program, monitor);
+				// Parse the pcheader structure
+				monitor.setMessage("Parsing Golang pcheader");
+				// We can use this to choose the appropriate version of the pcheader structure to apply
 				DataType pcheader_magic_datatype = getGolangPclntabMagicEnumDataType(program);
+
 				DataType pcheader_struct = this.getGolangPcheaderStructureDataType(program); 
 				Symbol runtime_pclntab = program.getSymbolTable().getSymbols("_runtime.pclntab").next();
 				Address pcheader_address = runtime_pclntab.getAddress();
+				Data existing_pcheader_data = program.getListing().getDataAt(pcheader_address);
 
+				if (existing_pcheader_data != null) {
+					log.appendMsg("Data already exist at pcheader location. Clearing to make room for the pcheader");
+					api.clearListing(pcheader_address, pcheader_address.add(pcheader_struct.getLength()));
+				}
+
+				// Extract the values from the structure and add xrefs to discover things and allow
+				// other analysis modules to use these to find things like functions and strings.
 				Data pcheader = program.getListing().createData(pcheader_address, pcheader_struct);
 				// TODO: Iterate over the components and find the correct field
 				Scalar text_start_field_value = (Scalar) pcheader.getComponent(7).getValue();
@@ -168,6 +190,8 @@ public class GolangFunctionAnalyzer implements Analyzer {
 				Address text_start_address = pcheader.getAddress().add(text_start_field_value.getValue());
 
 				program.getReferenceManager().addMemoryReference(text_start_field_address, text_start_address, RefType.DATA, SourceType.ANALYSIS, 0);
+
+				// Parse the module info table
 
 			} catch (Exception e) {
 				// do nothing because we are bad
